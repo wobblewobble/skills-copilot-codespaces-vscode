9b162d13010579ddf8d45881958cf7ab0cf91e0c
wobblewobble <mcglynn.joe@gmail.com>
2023-07-30 11:48:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    var contactBtn = document.getElementById("contactBtn");
+    var aboutBtn = document.getElementById("aboutBtn");
+    var homeBtn = document.getElementById("homeBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+    memberBtn.style.backgroundColor = "#333";
+    skillsBtn.style.backgroundColor = "#111";
+    contactBtn.style.backgroundColor = "#111";
+    aboutBtn.style.backgroundColor = "#111";
+    homeBtn.style.backgroundColor = "#111";
+}
